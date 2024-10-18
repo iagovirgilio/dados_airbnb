b430ad0f5cfd914f4713b7a908ba223babef2013
Iago Virgílio <iagovirgilio@gmail.com>
2024-10-18 10:55:05 -0300

    Add Icon para area de estadias

--- app/data_loader/apps.py
+++ app/data_loader/apps.py
@@ -5,3 +5,4 @@ class DataLoaderConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'data_loader'
     verbose_name = 'Estadias'
+    icon = 'fas fa-map-marker-alt'
